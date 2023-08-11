8fbe0ac5b886bd0a0c9218bf1b772411579702f0
ibrahim2375 <ibrahim.abdelrahman.abdelaziz@gmail.com>
2023-08-11 20:55:02 +0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { t } from 'react-native-tailwindcss';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={[t.absolute,t.top0]}>Open up App.js to start working on yours app!</Text>
       <StatusBar style="auto" />
     </View>
   );
