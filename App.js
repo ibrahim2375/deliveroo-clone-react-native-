5571169d422c22593e882c37081d6652d2cc0446
ibrahim2375 <ibrahim.abdelrahman.abdelaziz@gmail.com>
2023-08-11 23:23:12 +0300

    up

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 // screens
@@ -16,12 +15,3 @@ export default function App() {
       </NavigationContainer>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
