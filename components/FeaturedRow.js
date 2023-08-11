f25195b45d7a0ce856d8e1bd05f6d70ed1023b07
ibrahim2375 <ibrahim.abdelrahman.abdelaziz@gmail.com>
2023-08-12 00:52:20 +0300

    rows added

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -1,14 +1,16 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
 //icons
 import { Feather } from "@expo/vector-icons";
 //styles
 import { Styles } from "../styles/FeaturedRowStyles";
+//components
+import ResturantCard from "./ResturantCard";
 
 const FeaturedRow = ({ id, title, description }) => {
   return (
     <View>
-       {/* Row Header */}
+      {/* Row Header */}
       <View style={Styles.rowHeaderView}>
         <View style={Styles.rowHeaderViewTop}>
           <Text style={Styles.rowHeaderViewTitle}>{title}</Text>
@@ -16,6 +18,99 @@ const FeaturedRow = ({ id, title, description }) => {
         </View>
         <Text style={Styles.rowHeaderViewDescription}>{description}</Text>
       </View>
+      <ScrollView
+        contentContainerStyle={{
+          paddingHorizontal: 15,
+        }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      >
+        {/* ResturantCard */}
+        <ResturantCard
+          id={0}
+          imageUrl={
+            "https://th.bing.com/th/id/R.a7e6e749e18a9102a49bba450d5d477f?rik=y6o8m9C2ltevWA&riu=http%3a%2f%2fblog.hdwallsource.com%2fwp-content%2fuploads%2f2015%2f01%2ffood-wallpaper-5777-5942-hd-wallpapers.jpg&ehk=VbAvSVkflThjPWBkjqFB3Z1vjps1ZyRqUR8boO0gS80%3d&risl=&pid=ImgRaw&r=0"
+          }
+          title={"Yo! Suso"}
+          rating={4.8}
+          genre={"egyption"}
+          address={"123 maan st."}
+          description={"yes its short description"}
+          dishes={[]}
+          long={22}
+          lat={77}
+        />
+        <ResturantCard
+          id={0}
+          imageUrl={
+            "https://th.bing.com/th/id/R.a7e6e749e18a9102a49bba450d5d477f?rik=y6o8m9C2ltevWA&riu=http%3a%2f%2fblog.hdwallsource.com%2fwp-content%2fuploads%2f2015%2f01%2ffood-wallpaper-5777-5942-hd-wallpapers.jpg&ehk=VbAvSVkflThjPWBkjqFB3Z1vjps1ZyRqUR8boO0gS80%3d&risl=&pid=ImgRaw&r=0"
+          }
+          title={"Yo! Suso"}
+          rating={4.8}
+          genre={"egyption"}
+          address={"123 maan st."}
+          description={"yes its short description"}
+          dishes={[]}
+          long={22}
+          lat={77}
+        />
+        <ResturantCard
+          id={0}
+          imageUrl={
+            "https://th.bing.com/th/id/R.a7e6e749e18a9102a49bba450d5d477f?rik=y6o8m9C2ltevWA&riu=http%3a%2f%2fblog.hdwallsource.com%2fwp-content%2fuploads%2f2015%2f01%2ffood-wallpaper-5777-5942-hd-wallpapers.jpg&ehk=VbAvSVkflThjPWBkjqFB3Z1vjps1ZyRqUR8boO0gS80%3d&risl=&pid=ImgRaw&r=0"
+          }
+          title={"Yo! Suso"}
+          rating={4.8}
+          genre={"egyption"}
+          address={"123 maan st."}
+          description={"yes its short description"}
+          dishes={[]}
+          long={22}
+          lat={77}
+        />
+        <ResturantCard
+          id={0}
+          imageUrl={
+            "https://th.bing.com/th/id/R.a7e6e749e18a9102a49bba450d5d477f?rik=y6o8m9C2ltevWA&riu=http%3a%2f%2fblog.hdwallsource.com%2fwp-content%2fuploads%2f2015%2f01%2ffood-wallpaper-5777-5942-hd-wallpapers.jpg&ehk=VbAvSVkflThjPWBkjqFB3Z1vjps1ZyRqUR8boO0gS80%3d&risl=&pid=ImgRaw&r=0"
+          }
+          title={"Yo! Suso"}
+          rating={4.8}
+          genre={"egyption"}
+          address={"123 maan st."}
+          description={"yes its short description"}
+          dishes={[]}
+          long={22}
+          lat={77}
+        />
+        <ResturantCard
+          id={0}
+          imageUrl={
+            "https://th.bing.com/th/id/R.a7e6e749e18a9102a49bba450d5d477f?rik=y6o8m9C2ltevWA&riu=http%3a%2f%2fblog.hdwallsource.com%2fwp-content%2fuploads%2f2015%2f01%2ffood-wallpaper-5777-5942-hd-wallpapers.jpg&ehk=VbAvSVkflThjPWBkjqFB3Z1vjps1ZyRqUR8boO0gS80%3d&risl=&pid=ImgRaw&r=0"
+          }
+          title={"Yo! Suso"}
+          rating={4.8}
+          genre={"egyption"}
+          address={"123 maan st."}
+          description={"yes its short description"}
+          dishes={[]}
+          long={22}
+          lat={77}
+        />
+        <ResturantCard
+          id={0}
+          imageUrl={
+            "https://th.bing.com/th/id/R.a7e6e749e18a9102a49bba450d5d477f?rik=y6o8m9C2ltevWA&riu=http%3a%2f%2fblog.hdwallsource.com%2fwp-content%2fuploads%2f2015%2f01%2ffood-wallpaper-5777-5942-hd-wallpapers.jpg&ehk=VbAvSVkflThjPWBkjqFB3Z1vjps1ZyRqUR8boO0gS80%3d&risl=&pid=ImgRaw&r=0"
+          }
+          title={"Yo! Suso"}
+          rating={4.8}
+          genre={"egyption"}
+          address={"123 maan st."}
+          description={"yes its short description"}
+          dishes={[]}
+          long={22}
+          lat={77}
+        />
+      </ScrollView>
     </View>
   );
 };
--- styles/FeaturedRowStyles.js
+++ styles/FeaturedRowStyles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 export const Styles = StyleSheet.create({
   rowHeaderView: {
     marginTop: 20,
-    paddingHorizontal: 10,
+    paddingHorizontal: 15,
   },
   rowHeaderViewTop: {
     flexDirection: "row",
@@ -12,9 +12,10 @@ export const Styles = StyleSheet.create({
   },
   rowHeaderViewTitle: {
     fontWeight: "bold",
+    fontSize: 18,
   },
   rowHeaderViewDescription: {
     fontSize: 12,
-    color: "#ccc",
+    color: "gray",
   },
 });
